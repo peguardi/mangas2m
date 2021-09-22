6965d4dd96e9735154cc90d515b8d46958c7cf5c
Peter Araque <peter.araque@gmail.com>
2021-09-21 21:11:28 -0400

    Agergadas funcionalidad definitiva al menú y algunos estilos más

--- js/app.js
+++ js/app.js
@@ -5,57 +5,75 @@ window.addEventListener('scroll', function () {
 });
 
 // SELECCIÓN ACTIVA EN MENÚ DE NAVEGACIÓN POR SCROLL **************************
-addEventListener('DOMContentLoaded', () => {
-  const secciones = document.querySelectorAll('.seccion');
-  const menuItems = document.querySelectorAll('.nav-link');
-
-  const funcionObserver = (entries) => {
-    entries.forEach((entry) => {
-      if (entry.isIntersecting) {
-        const itemActual = Array.from(menuItems).find(
-          (item) => item.dataset.url === entry.target.id
-        );
-        itemActual.classList.add('active');
-        for (const item of menuItems) {
-          if (item != itemActual) {
-            item.classList.remove('active');
-          }
-        }
-      }
-    });
-  };
-
-  const observer = new IntersectionObserver(funcionObserver, {
-    root: null,
-    rootMargin: '0px',
-    threshold: 0.8,
+const sections = document.querySelectorAll('section');
+const navLi = document.querySelectorAll('nav ul li');
+window.addEventListener('scroll', () => {
+  let current = '';
+  sections.forEach((section) => {
+    const sectionTop = section.offsetTop;
+    const sectionHeight = section.clientHeight;
+    if (scrollY >= sectionTop - sectionHeight / 4) {
+      current = section.getAttribute('id');
+    }
   });
 
-  secciones.forEach((seccion) => observer.observe(seccion));
-});
-
-// DESPLIEGUE DE OVERLAY EN MENÚ RESPONSIVE **************************
-
-document.addEventListener('DOMContentLoaded', () => {
-  // const nav = document.querySelector('header');
-
-  // document.querySelector('#check').addEventListener('click', () => {
-  //   nav.classList.add('nav--open');
-  // });
-
-  // document.querySelector('.nav__overlay').addEventListener('click', () => {
-  //   nav.classList.remove('nav--open');
-  // });
-
-  const nav = document.querySelector('header');
-  let menuOpen = false;
-  document.querySelector('#check').addEventListener('click', () => {
-    if (!menuOpen) {
-      nav.classList.add('nav--open');
-      menuOpen = true;
-    } else {
-      nav.classList.remove('nav--open');
-      menuOpen = false;
+  navLi.forEach((li) => {
+    li.classList.remove('active');
+    if (li.classList.contains(current)) {
+      li.classList.add('active');
     }
   });
 });
+
+/* Loop through all dropdown buttons to toggle between hiding and showing its dropdown content - This allows the user to have multiple dropdowns without any conflict */
+var dropdown = document.getElementsByClassName('dropdown-btn');
+var i;
+
+for (i = 0; i < dropdown.length; i++) {
+  dropdown[i].addEventListener('click', function () {
+    // this.classList.toggle('active');
+    var dropdownContent = this.nextElementSibling;
+    if (dropdownContent.style.display === 'block') {
+      dropdownContent.style.display = 'none';
+    } else {
+      dropdownContent.style.display = 'block';
+    }
+  });
+}
+
+// ANIMACIÓN Y FUNCIONALIDAD DEL BOTÓN DE MENÚ **************************
+const menuBtn = document.querySelector('.menuBtn');
+const menuNav = document.querySelector('.navMenu');
+let menuOpen = false;
+menuBtn.addEventListener('click', () => {
+  if (!menuOpen) {
+    menuBtn.classList.add('change');
+    menuOpen = true;
+    openNav();
+    document.getElementById('overlay').style.display = 'block';
+  } else {
+    menuBtn.classList.remove('change');
+    menuOpen = false;
+    closeNav();
+    document.getElementById('overlay').style.display = 'none';
+  }
+});
+
+function cierraBtn() {
+  if (window.innerWidth < 769) {
+    menuBtn.classList.remove('change');
+    menuOpen = false;
+    closeNav();
+    document.getElementById('overlay').style.display = 'none';
+  }
+  document.getElementById('dropContent1').style.display = 'none';
+  document.getElementById('dropContent2').style.display = 'none';
+}
+
+function openNav() {
+  menuNav.classList.add('change');
+}
+
+function closeNav() {
+  menuNav.classList.remove('change');
+}
