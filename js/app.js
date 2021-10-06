97b9c417cf434313198eef7090e153266a5220ed
Peter Araque <peter.araque@gmail.com>
2021-10-05 20:21:21 -0400

    Finalización del contenido de la página web

--- js/app.js
+++ js/app.js
@@ -77,3 +77,29 @@ function openNav() {
 function closeNav() {
   menuNav.classList.remove('change');
 }
+
+// Get the modal
+var modal = document.getElementById('myModal');
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.getElementsByClassName('myImg');
+var modalImg = document.getElementById('img01');
+var captionText = document.getElementById('caption');
+var i;
+
+for (i = 0; i < img.length; i++) {
+  img[i].addEventListener('click', function () {
+    // img.onclick = function () {
+    modal.style.display = 'block';
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+  });
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName('close')[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+  modal.style.display = 'none';
+};
